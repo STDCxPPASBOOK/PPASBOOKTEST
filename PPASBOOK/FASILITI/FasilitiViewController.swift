1749cbf957cc2fa44c144829e0a1c87d51bf4408
Xrezo <141310481+Xrezo@users.noreply.github.com>
2024-07-09 12:10:41 +0800

    try

--- PPASBOOK/FASILITI/FasilitiViewController.swift
+++ PPASBOOK/FASILITI/FasilitiViewController.swift
@@ -7,29 +7,24 @@ class FasilitiViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet var backButton: UIBarButtonItem!
     @IBOutlet var searchBar: UISearchBar!
-    @IBOutlet var buttonFilter: UIButton!
-    @IBOutlet var buttonFB: UIButton!
-    @IBOutlet var buttonPM: UIButton!
     
     var dataSource: UICollectionViewDiffableDataSource<Section, YourDataModel>!
-    var items = [
-        YourDataModel(imageName: "slide1", label1Text: "Auditorium", label2Text: "👥 200 Pax(Maksimum)", label3Text: "💵 RM 330/Jam"),
-        YourDataModel(imageName: "slide1", label1Text: "Dewan Serbaguna", label2Text: "👥 200 Pax(Maksimum)", label3Text: "💵 RM 250/Jam"),
-        YourDataModel(imageName: "slide1", label1Text: "Bilik Seminar 4", label2Text: "👥 70 Pax(Maksimum)", label3Text: "💵 RM 200/Jam"),
-        YourDataModel(imageName: "slide1", label1Text: "Bilik Kaca 1", label2Text: "👥 55 Pax(Maksimum)", label3Text: "💵 RM 80/Jam"),
-        YourDataModel(imageName: "slide1", label1Text: "Bilik Aktiviti Kanak-Kanak", label2Text: "👥 40 Pax(Maksimum)", label3Text: "💵 RM 60/Jam"),
-        YourDataModel(imageName: "slide1", label1Text: "Bilik Mesyuarat", label2Text: "👥 40 Pax(Maksimum)", label3Text: "💵 RM 80/Jam"),
-        YourDataModel(imageName: "slide1", label1Text: "Bilik Latihan", label2Text: "👥 28 Pax(Maksimum)", label3Text: "💵 RM 75/Jam"),
-        YourDataModel(imageName: "slide1", label1Text: "Makmal IT", label2Text: "👥 80 Pax(Maksimum)", label3Text: "💵 RM 80/Jam"),
-        YourDataModel(imageName: "slide1", label1Text: "Bilik Perbincangan 1", label2Text: "👥 20 Pax(Maksimum)", label3Text: "💵 RM 50/Jam"),
-        YourDataModel(imageName: "slide1", label1Text: "Bilik Perbincangan 2", label2Text: "👥 10 Pax(Maksimum)", label3Text: "💵 RM 40/Jam"),
-        YourDataModel(imageName: "slide1", label1Text: "Bilik Taklimat", label2Text: "👥 40 Pax(Maksimum)", label3Text: "💵 RM 70/Jam"),
+    var items: [YourDataModel] = [
+        YourDataModel(imageName: "slide1", label1Text: "Bilik Auditorium", label2Text: "200 Pax", label3Text: "RM 330/Jam"),
+        YourDataModel(imageName: "slide1", label1Text: "Dewan Serbaguna", label2Text: "200 Pax", label3Text: "RM 250/Jam"),
+        YourDataModel(imageName: "slide1", label1Text: "Bilik Seminar 4", label2Text: "70 Pax", label3Text: "RM 200/Jam"),
+        YourDataModel(imageName: "slide1", label1Text: "Bilik Kaca 1", label2Text: "55 Pax", label3Text: "RM 80/Jam"),
+        YourDataModel(imageName: "slide1", label1Text: "Bilik Aktiviti Kanak-Kanak", label2Text: "40 Pax", label3Text: "RM 60/Jam"),
+        YourDataModel(imageName: "slide1", label1Text: "Bilik Mesyuarat", label2Text: "40 Pax", label3Text: "RM 80/Jam"),
+        YourDataModel(imageName: "slide1", label1Text: "Bilik Latihan", label2Text: "28 Pax", label3Text: "RM 75/Jam"),
+        YourDataModel(imageName: "slide1", label1Text: "Makmal IT", label2Text: "24 Pax", label3Text: "RM 80/Jam"),
+        YourDataModel(imageName: "slide1", label1Text: "Bilik Perbincangan 1", label2Text: "20 Pax", label3Text: "RM 50/Jam"),
+        YourDataModel(imageName: "slide1", label1Text: "Bilik Perbincangan 2", label2Text: "10 Pax", label3Text: "RM 40/Jam"),
+        YourDataModel(imageName: "slide1", label1Text: "Bilik Taklimat", label2Text: "40 Pax", label3Text: "RM 70/Jam"),
+        // Tambah lagi data di sini
     ]
     
     var filteredItems: [YourDataModel] = []
-    var isFiltering: Bool {
-        return !searchBar.text!.isEmpty
-    }
     
     enum Section: CaseIterable {
         case main
@@ -46,31 +41,27 @@ class FasilitiViewController: UIViewController {
         createDataSource()
         
         // Apply initial snapshot
-        applySnapshot()
+        applySnapshot(items: items)
         
         // Setup search bar delegate
         searchBar.delegate = self
-        searchBar.showsCancelButton = true // Show cancel button
-        
-        // Add shadow to buttons
-        configureButtonShadows()
     }
     
     private func createDataSource() {
-        let cellRegistration = UICollectionView.CellRegistration<BasicCollectionViewCell, YourDataModel> { cell, indexPath, data in
-            cell.configure(with: data)
+        let cellRegistration = UICollectionView.CellRegistration<BasicCollectionViewCell, YourDataModel> { cell, indexPath, itemIdentifier in
+            cell.configure(with: itemIdentifier)
         }
         
-        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView) { collectionView, indexPath, data in
-            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: data)
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView) { collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
         }
     }
     
-    private func applySnapshot() {
+    private func applySnapshot(items: [YourDataModel], animatingDifferences: Bool = true) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, YourDataModel>()
         snapshot.appendSections([.main])
         snapshot.appendItems(items)
-        dataSource.apply(snapshot, animatingDifferences: true)
+        dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
     }
     
     private func generateLayout() -> UICollectionViewLayout {
@@ -100,6 +91,7 @@ class FasilitiViewController: UIViewController {
         return layout
     }
     
+<<<<<<< HEAD
     @IBAction func filterButtonTapped(_ sender: UIButton) {
         let alert = UIAlertController(title: "FILTER", message: "Masukkan julat harga:", preferredStyle: .alert)
         
@@ -188,6 +180,11 @@ class FasilitiViewController: UIViewController {
         buttonPM.addTarget(self, action: #selector(buttonPMTapped(_:)), for: .touchUpInside)
             buttonPM.backgroundColor = .clear // Set initial background color
     }
+=======
+    @IBAction func backButtonTapped(_ sender: UIBarButtonItem) {
+        navigationController?.popViewController(animated: true)
+    }
+>>>>>>> parent of 3557f04 (ok)
 }
 
 extension FasilitiViewController: UISearchBarDelegate {
@@ -197,24 +194,18 @@ extension FasilitiViewController: UISearchBarDelegate {
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.text = nil
-        filterContentForSearchText("") // Clear search filter
+        filterContentForSearchText("")
         searchBar.resignFirstResponder()
-        applySnapshot() // Apply initial snapshot to clear filters
     }
-}
-
-extension FasilitiViewController {
+    
     func filterContentForSearchText(_ searchText: String) {
-        filteredItems = items.filter { data in
+        filteredItems = items.filter { item in
             let lowercasedText = searchText.lowercased()
-            return data.label1Text.lowercased().contains(lowercasedText) ||
-                   data.label2Text.lowercased().contains(lowercasedText) ||
-                   data.label3Text.lowercased().contains(lowercasedText)
+            return item.label1Text.lowercased().contains(lowercasedText) ||
+                   item.label2Text.lowercased().contains(lowercasedText) ||
+                   item.label3Text.lowercased().contains(lowercasedText)
         }
         
-        var snapshot = NSDiffableDataSourceSnapshot<Section, YourDataModel>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(filteredItems)
-        dataSource.apply(snapshot, animatingDifferences: true)
+        applySnapshot(items: filteredItems)
     }
 }
