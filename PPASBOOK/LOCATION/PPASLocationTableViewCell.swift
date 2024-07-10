5234e0af1b5e3c73d3e524651b254a55ea8cd2ab
Xrezo <141310481+Xrezo@users.noreply.github.com>
2024-07-10 10:45:04 +0800

    ok

--- PPASBOOK/LOCATION/PPASLocationTableViewCell.swift
+++ PPASBOOK/LOCATION/PPASLocationTableViewCell.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class PPASLocationTableViewCell: UITableViewCell {
     
-        @IBOutlet weak var titleLabel: UILabel!
-        @IBOutlet weak var districtLabel: UILabel!
-        @IBOutlet weak var operationHoursLabel: UILabel!
-        @IBOutlet weak var statusLabel: UILabel!
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var districtLabel: UILabel!
+    @IBOutlet weak var operationHoursLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        districtLabel.translatesAutoresizingMaskIntoConstraints = false
+        operationHoursLabel.translatesAutoresizingMaskIntoConstraints = false
+        statusLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
 }
+
