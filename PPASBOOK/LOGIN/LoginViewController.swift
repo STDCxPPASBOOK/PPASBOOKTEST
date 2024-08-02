baf3a5a726429484c6267de7155cb8afac22b0ea
Xrezo <141310481+Xrezo@users.noreply.github.com>
2024-08-02 15:19:33 +0800

    banyak

--- PPASBOOK/LOGIN/LoginViewController.swift
+++ PPASBOOK/LOGIN/LoginViewController.swift
@@ -9,23 +9,24 @@ class LoginViewController: UIViewController {
     @IBOutlet var loginButton: UIButton!
     @IBOutlet var alertLabel: UILabel!
     @IBOutlet var newUser: UIButton!
-    @IBOutlet var togglePasswordButton: UIButton! // Add this outlet
-    
+    @IBOutlet var togglePasswordButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton! // Add this IBOutlet
+
     var alertMessage: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         noicTextField.clipToCircle()
         passwordTextField.clipToCircle()
-        
+
         loginButton.layer.cornerRadius = 10
         loginButton.layer.borderColor = UIColor.systemTeal.cgColor
         loginButton.layer.borderWidth = 1.0
         alertLabel.isHidden = true
-        
-        // Add target for toggle password button
+
         togglePasswordButton.addTarget(self, action: #selector(togglePasswordVisibility), for: .touchUpInside)
-        
+        forgotPasswordButton.addTarget(self, action: #selector(forgotPasswordTapped), for: .touchUpInside) // Add this line
+
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(tapGesture)
     }
@@ -33,84 +34,93 @@ class LoginViewController: UIViewController {
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         if let message = alertMessage {
             showAlert(message: message, isError: false)
-            alertMessage = nil // Clear the message after showing it
-        }
-    }
-    func saveUser(_ user: User) {
-        let db = Firestore.firestore()
-        db.collection("users").document(user.noic).setData([
-            "email": user.email,
-            "noic": user.noic,
-            "fullname": user.fullname,
-            "contactNumber": user.contactNumber
-        ]) { err in
-            if let err = err {
-                print("Error writing document: \(err)")
-            } else {
-                print("Document successfully written!")
-            }
+            alertMessage = nil
         }
     }
 
     @IBAction func loginTapped(_ sender: UIButton) {
-           guard let noic = noicTextField.text, !noic.isEmpty,
-                 let password = passwordTextField.text, !password.isEmpty else {
-               showAlert(message: "Sila isi semua maklumat.")
-               return
-           }
-           
-           guard isNumeric(noic) else {
-               showAlert(message: "Nombor IC mesti mengandungi nombor sahaja.")
-               return
-           }
+        guard let noic = noicTextField.text, !noic.isEmpty,
+              let password = passwordTextField.text, !password.isEmpty else {
+            showAlert(message: "Sila isi semua maklumat.")
+            return
+        }
 
-           Auth.auth().signIn(withEmail: noic + "@ppasbook.com", password: password) { [weak self] authResult, error in
-               guard let strongSelf = self else { return }
-               if let error = error {
-                   strongSelf.showAlert(message: error.localizedDescription)
-               } else {
-                   strongSelf.showAlert(message: "Login Berjaya!", isError: false)
-                   DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                       strongSelf.navigateToProfile()
-                   }
-               }
-           }
-       }
+        guard isNumeric(noic) else {
+            showAlert(message: "Nombor IC mesti mengandungi nombor sahaja.")
+            return
+        }
 
-       @objc func togglePasswordVisibility() {
-           passwordTextField.isSecureTextEntry.toggle()
-           
-           let buttonImageName = passwordTextField.isSecureTextEntry ? "eye.fill" : "eye.slash.fill"
-           togglePasswordButton.setImage(UIImage(systemName: buttonImageName), for: .normal)
-       }
-       
-       func showAlert(message: String, isError: Bool = true) {
-           alertLabel.text = message
-           alertLabel.textColor = isError ? .red : .green
-           alertLabel.isHidden = false
-       }
+        let email = noic + "@ppasbook.com"
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+            guard let strongSelf = self else { return }
+            if let error = error {
+                strongSelf.showAlert(message: error.localizedDescription)
+            } else {
+                strongSelf.showAlert(message: "Login Berjaya!", isError: false)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    strongSelf.navigateToProfile()
+                }
+            }
+        }
+    }
 
-       func isNumeric(_ string: String) -> Bool {
-           return !string.isEmpty && string.allSatisfy { $0.isNumber }
-       }
-       
-       func navigateToProfile() {
-           let storyboard = UIStoryboard(name: "MainPage", bundle: nil)
-           let tabBarController = storyboard.instantiateViewController(withIdentifier: "MainTabBarController") as! UITabBarController
-                  tabBarController.selectedIndex = 1
-           if let profileVC = storyboard.instantiateViewController(withIdentifier: "MainTabBarController") as? MainTabBarController {
-               profileVC.modalPresentationStyle = .fullScreen
-               profileVC.modalTransitionStyle = .crossDissolve
-               self.present(profileVC, animated: true, completion: nil)
-           }
-           
-       }
-   }
+    @objc func togglePasswordVisibility() {
+        passwordTextField.isSecureTextEntry.toggle()
+
+        let buttonImageName = passwordTextField.isSecureTextEntry ? "eye.fill" : "eye.slash.fill"
+        togglePasswordButton.setImage(UIImage(systemName: buttonImageName), for: .normal)
+    }
+
+    @objc func forgotPasswordTapped() {
+        let alertController = UIAlertController(title: "Forgot Password", message: "Enter your email to reset your password.", preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.placeholder = "Email"
+        }
+        let resetAction = UIAlertAction(title: "Reset", style: .default) { _ in
+            if let email = alertController.textFields?.first?.text, !email.isEmpty {
+                Auth.auth().sendPasswordReset(withEmail: email) { error in
+                    if let error = error {
+                        self.showAlert(message: error.localizedDescription)
+                    } else {
+                        self.showAlert(message: "Password reset email sent.", isError: false)
+                    }
+                }
+            } else {
+                self.showAlert(message: "Please enter an email address.")
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(resetAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
+    func showAlert(message: String, isError: Bool = true) {
+        alertLabel.text = message
+        alertLabel.textColor = isError ? .red : .green
+        alertLabel.isHidden = false
+    }
+
+    func isNumeric(_ string: String) -> Bool {
+        return !string.isEmpty && string.allSatisfy { $0.isNumber }
+    }
+
+    func navigateToProfile() {
+        let storyboard = UIStoryboard(name: "MainPage", bundle: nil)
+        let tabBarController = storyboard.instantiateViewController(withIdentifier: "MainTabBarController") as! UITabBarController
+        tabBarController.selectedIndex = 1
+        if let profileVC = storyboard.instantiateViewController(withIdentifier: "MainTabBarController") as? MainTabBarController {
+            profileVC.modalPresentationStyle = .fullScreen
+            profileVC.modalTransitionStyle = .crossDissolve
+            self.present(profileVC, animated: true, completion: nil)
+        }
+    }
+}
 
 extension UITextField {
     func clipToCircle() {
@@ -121,3 +131,5 @@ extension UITextField {
         self.clipsToBounds = true
     }
 }
+
+
