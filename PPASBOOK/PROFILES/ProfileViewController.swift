f4f272e55fc14e48f3e585b0b6a864a03e21d15a
CpuL04 <fredoidola@gmail.com>
2024-08-05 16:04:55 +0800

    ok

--- PPASBOOK/PROFILES/ProfileViewController.swift
+++ PPASBOOK/PROFILES/ProfileViewController.swift
@@ -1,32 +1,48 @@
-//
-//  ProfileViewController.swift
-//  PPASBOOK
-//
-//  Created by STDC_15 on 10/07/2024.
-//
-
 import UIKit
 import Firebase
 import QuartzCore
 
-class ProfileViewController: UIViewController {
-    
+class ProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
     @IBOutlet var tb: UITableView!
     @IBOutlet var logoutButton: UIButton!
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var icLabel: UILabel!
+    @IBOutlet var imageChange: UIImageView!
     
     var user: User? // Add a property to hold the user data
+
+    let menuItems = [
+        ("Settings & Privacy"),
+        ("History"),
+        ("Language"),
+        ("How to use PPASBOOK"),
+        ("Social Media")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tb.layer.cornerRadius = 10
         tb.clipsToBounds = true
-
+        
+        logoutButton.layer.cornerRadius = 10
+        logoutButton.clipsToBounds = true
+        logoutButton.layer.shadowRadius = 20
+        logoutButton.layer.shadowOpacity = 20
         logoutButton.addTarget(self, action: #selector(logoutButtonTapped), for: .touchUpInside)
         
+        imageChange.layer.cornerRadius = imageChange.frame.height / 2
+        
+        tb.dataSource = self
+        tb.delegate = self
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
+        imageChange.isUserInteractionEnabled = true
+        imageChange.addGestureRecognizer(tapGestureRecognizer)
+        
         fetchUserProfile()
+        loadProfileImage() // Load the profile image from UserDefaults
     }
     
     @objc func logoutButtonTapped() {
@@ -74,16 +90,63 @@ class ProfileViewController: UIViewController {
         nameLabel.text = user.fullname
         icLabel.text = user.noic
     }
-}
 
+    // MARK: - UITableViewDataSource Methods
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
     }
-    */
-//}
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath)
+        let menuItem = menuItems[indexPath.row]
+        cell.textLabel?.text = menuItem
+        return cell
+    }
+
+    // MARK: - UITableViewDelegate Methods
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Implement navigation or action based on selected row
+        // Example:
+        // if indexPath.row == 0 {
+        //     // Navigate to Edit Profile screen
+        // }
+    }
+
+    // MARK: - UIImagePickerControllerDelegate Methods
+
+    @objc func imageTapped() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
+        present(imagePickerController, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            imageChange.image = selectedImage
+            saveProfileImage(image: selectedImage) // Save the selected image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    // MARK: - Helper Methods
+
+    func saveProfileImage(image: UIImage) {
+        if let imageData = image.jpegData(compressionQuality: 0.8) {
+            UserDefaults.standard.set(imageData, forKey: "profileImage")
+        }
+    }
+
+    func loadProfileImage() {
+        if let imageData = UserDefaults.standard.data(forKey: "profileImage") {
+            imageChange.image = UIImage(data: imageData)
+        }
+    }
+}
